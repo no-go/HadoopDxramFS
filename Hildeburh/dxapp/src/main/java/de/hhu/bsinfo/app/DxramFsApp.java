e5cdeab9870e20ce3d79b51adbc0a1cef6127cf1
tux@chuwi <no-go@localhost>
2018-08-25 18:44:33 +0200

    todo: to test the gradle dxram stuff and dxnet, I have to add my modified dxnet into the new dxram. Still a problem in my old code: chunk creating, storing or accesssing stil with ugly fail.

--- Hildeburh/dxapp/src/main/java/de/hhu/bsinfo/app/DxramFsApp.java
+++ Hildeburh/dxapp/src/main/java/de/hhu/bsinfo/app/DxramFsApp.java
@@ -2,6 +2,8 @@ package de.hhu.bsinfo.app;
 
 import com.google.gson.annotations.Expose;
 
+import java.util.ArrayList;
+
 import de.hhu.bsinfo.dxnet.core.NetworkException;
 import de.hhu.bsinfo.dxram.DXRAM;
 import de.hhu.bsinfo.dxram.app.AbstractApplication;
@@ -9,9 +11,12 @@ import de.hhu.bsinfo.dxram.boot.BootService;
 import de.hhu.bsinfo.dxram.chunk.ChunkService;
 import de.hhu.bsinfo.dxram.nameservice.NameserviceService;
 import de.hhu.bsinfo.dxram.engine.DXRAMVersion;
+import de.hhu.bsinfo.app.dxramfspeer.*;
 import de.hhu.bsinfo.app.dxramfscore.*;
 import de.hhu.bsinfo.app.dxramfscore.rpc.*;
 import de.hhu.bsinfo.dxutils.NodeID;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import de.hhu.bsinfo.dxram.generated.BuildConfig;
 
 public class DxramFsApp extends AbstractApplication {
@@ -37,7 +42,20 @@ public class DxramFsApp extends AbstractApplication {
     @Expose
     private int max_pathlength_chars = 256;
 
+    // ++++++++ --------------------------
 
+    private static final Logger LOG = LogManager.getFormatterLogger(DxramFsApp.class.getSimpleName());
+
+    private long ROOT_CID;
+    private BootService bootS;
+    private ChunkService chunkS;
+    private NameserviceService nameS;
+
+    private FsNodeChunk ROOTN;
+    private DxnetInit dxnetInit;
+    
+    public static NodePeerConfig nopeConfig;
+    
     @Override
     public DXRAMVersion getBuiltAgainstVersion() {
         return BuildConfig.DXRAM_VERSION;
@@ -55,12 +73,50 @@ public class DxramFsApp extends AbstractApplication {
 
     @Override
     public void main() {
-        BootService bootService = getService(BootService.class);
+        bootS = getService(BootService.class);
+        chunkS = getService(ChunkService.class);
+        nameS = getService(NameserviceService.class);
 
-        System.out.println("Hello, I am application " + getApplicationName() + " on a peer and my node id is " +
-                NodeID.toHexString(bootService.getNodeID()));
+        System.out.println(
+                "application " + getApplicationName() + " on a peer" +
+                        " and my dxram node id is " + NodeID.toHexString(bootS.getNodeID())
+        );
+        System.out.println("Where your Hadoop Node is:");
+        System.out.println("DXNET dxnet_local_id  : " + dxnet_local_id);
+        System.out.println("DXNET dxnet_local_addr: " + dxnet_local_addr);
+        System.out.println("DXNET dxnet_local_port: " + dxnet_local_port);
 
-        // Put your application code running on the DXRAM node/peer here
+        nopeConfig = new NodePeerConfig();
+        nopeConfig.nodeId = (short) dxnet_local_id;
+        nopeConfig.addr = dxnet_local_addr;
+        nopeConfig.port = dxnet_local_port;
+
+        nopeConfig.dxPeers = new ArrayList<>();
+
+        // @todo if you realy want to run many dxram/dxnet peers on local, you have to change this !!
+        System.out.println("How Hadoop DxramFs has to connect me:");
+        System.out.println("DXNET dxnet_local_peer_id  : " + dxnet_local_peer_id);
+        System.out.println("DXNET dxnet_local_peer_addr: " + dxnet_local_peer_addr);
+        System.out.println("DXNET dxnet_local_peer_port: " + dxnet_local_peer_port);
+        NodePeerConfig.PeerConfig aPeer = new NodePeerConfig.PeerConfig();
+        aPeer.nodeId = (short) dxnet_local_peer_id;
+        aPeer.addr = dxnet_local_peer_addr;
+        aPeer.port = dxnet_local_peer_port;
+        nopeConfig.dxPeers.add(aPeer);
+
+        DxramFsConfig.file_blocksize = file_blocksize;
+        DxramFsConfig.ref_ids_each_fsnode = ref_ids_each_fsnode;
+        DxramFsConfig.max_pathlength_chars = max_pathlength_chars;
+
+        dxnetInit = new DxnetInit(nopeConfig, nopeConfig.dxPeers.get(0).nodeId);
+        
+        while (true) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ignored) {
+                
+            }
+        }
     }
 
     @Override
